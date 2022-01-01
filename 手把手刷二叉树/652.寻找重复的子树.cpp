6dd9f537d14ede7b91b20ba4bdccf65d3e7a1b1a
gipbear <1099048889@qq.com>
2022-01-01 23:54:54 +0800

    二叉树第三期

---
+++ 手把手刷二叉树/652.寻找重复的子树.cpp
@@ -0,0 +1,56 @@
+#include<vector>
+#include<string>
+#include<unordered_map>
+using namespace std;
+
+/*
+ * @lc app=leetcode.cn id=652 lang=cpp
+ *
+ * [652] 寻找重复的子树
+ */
+
+//Definition for a binary tree node.
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// @lc code=start
+
+class Solution {
+public:
+
+    unordered_map<string, int> memo;
+    vector<TreeNode*> res;
+
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        traverse(root);
+        return res;
+    }
+
+    string traverse(TreeNode* root){
+        if(root == nullptr){
+            return "#";
+        }
+
+        // 为了清楚树的结构，对该树序列化
+        // 此处选择使用后序遍历
+        string subTree = traverse(root->left) +"," + traverse(root->right) + "," + to_string(root->val);
+
+        // 等于1时说明之前已经存在过了，
+        // 而且之后还有的重复的也只计算一次
+        if(memo[subTree] == 1){
+            res.push_back(root);
+        }
+        memo[subTree]++;
+
+        return subTree;
+    }
+};
+// @lc code=end
+
